a7a2c70e93062127d0514eaedf30898eafa76daa
Michael Mansperger <ManspergerMichael@gmail.com>
2019-02-28 18:03:35 -0800

    currency component

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  title = 'Crazy Uncle Mikes Formatting and Sex Dungeon';
 }
