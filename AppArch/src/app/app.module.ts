9b5f3eef7aa106a2b862b6c34d47ad6bca188727
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-06 14:06:46 -0800

    starting over

--- AppArch/src/app/app.module.ts
+++ AppArch/src/app/app.module.ts
@@ -3,20 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskComponentComponent } from './task-component/task-component.component';
-import { TaskListComponentComponent } from './task-list-component/task-list-component.component';
-import { TaskNewComponentComponent } from './task-new-component/task-new-component.component';
-import { TaskNewComponntComponent } from './task-component/task-new-componnt/task-new-componnt.component';
-import { TaskServiceComponent } from './task-service/task-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TaskComponentComponent,
-    TaskListComponentComponent,
-    TaskNewComponentComponent,
-    TaskNewComponntComponent,
-    TaskServiceComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
