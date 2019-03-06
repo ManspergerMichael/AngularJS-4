6a8da19c1e03392ab3f6c50e67e84b10ff114514
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-06 12:01:51 -0800

    notes

--- pipes/src/app/curency/curency.component.ts
+++ pipes/src/app/curency/curency.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurencyComponent implements OnInit {
   value: number;
-  
+  //functions also go here
+
   constructor() { 
     
   }
