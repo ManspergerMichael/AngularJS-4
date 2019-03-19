1766991f58cb875222cb083fd9663390f8d08e7d
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-19 15:11:58 -0700

    model and front end stuff

--- Users/src/app/user/user.ts
+++ Users/src/app/user/user.ts
@@ -1,2 +1,11 @@
 export class User {
+    name: string;
+    createdAt: Date;
+    updatedAt: Date;
+
+    constructor(name){
+        this.name = name;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
 }
