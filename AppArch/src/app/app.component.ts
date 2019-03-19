7a8820ec306e99542ea1657bd35521aad5094cfd
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-18 18:07:02 -0700

    some changes

--- AppArch/src/app/app.component.ts
+++ AppArch/src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   title = 'AppArch';
   tasks: Array<any>;
   constructor(private _taskService: TaskServiceService){
-   
+    this.tasks = this._taskService.tasks
   }
   ngOnInit(){
     console.log(this._taskService);
