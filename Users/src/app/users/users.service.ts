0a9b1610a006e1ff6d9a06d4867e6a2f1676ab75
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-19 17:35:09 -0700

    model and service complete
    service is also registered

--- Users/src/app/users/users.service.ts
+++ Users/src/app/users/users.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { User } from './user'
+import { Http } from '@angular/http';
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  users: Array<User> = [
+    new User("Bojack")
+  ];
 
-  constructor() { }
+  constructor(private _http: Http) 
+  {
+    console.log(_http);    
+   }
 }
