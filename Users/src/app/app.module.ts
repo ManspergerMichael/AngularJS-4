fff0459224b7e3e39b11609944db0197d94852a4
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-19 17:27:20 -0700

    Retrying again
    Source of problem: I forgot to install the http libary
    npm install @angular/http

--- Users/src/app/app.module.ts
+++ Users/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UsersService } from './users/users.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
