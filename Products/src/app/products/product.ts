06ed04bfda91f85b3550a5cfac19351c0807c975
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-27 16:49:17 -0700

    added css and created product class

--- Products/src/app/products/product.ts
+++ Products/src/app/products/product.ts
@@ -1,2 +1,19 @@
 export class Product {
+    name:String;
+    description:String;
+    price:number;
+    quantity:number;
+    date_created: Date;
+    date_updated: Date;
+
+    constructor(name, description, price, quantity)
+    {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.date_created = new Date;
+        this.date_updated = new Date;
+    }
+
 }
