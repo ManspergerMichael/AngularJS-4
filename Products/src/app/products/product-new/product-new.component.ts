f044d93f4c0b4bceacfc709acb0549288f121b73
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-27 17:56:09 -0700

    Stopping for now
    Next I will include functionality to add new products to the product service array

--- Products/src/app/products/product-new/product-new.component.ts
+++ Products/src/app/products/product-new/product-new.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductServiceService } from '../product-service.service';
 @Component({
   selector: 'app-product-new',
   templateUrl: './product-new.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductNewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _products: ProductServiceService) { }
 
   ngOnInit() {
+  }
+  update(){
+    
   }
 
 }
