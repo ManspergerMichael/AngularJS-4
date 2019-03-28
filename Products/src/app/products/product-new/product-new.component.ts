f0043898a339a86b955b67677a43949528a29b77
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-28 11:17:41 -0700

    learning forms

--- Products/src/app/products/product-new/product-new.component.ts
+++ Products/src/app/products/product-new/product-new.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductServiceService } from '../product-service.service';
+//importing product lcass to create new object and send to service
+import { Product } from '../product';
 @Component({
   selector: 'app-product-new',
   templateUrl: './product-new.component.html',
